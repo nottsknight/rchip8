36e718b4fbef6fe26f81563d484dcd2575057bd5
Ian Knight <ian.knight2@nottingham.ac.uk>
2023-02-11 18:39:32 +0000

    Add more sophisticated test rom

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use rchip8::machine::VirtualMachine;
 
 fn main() {
     let mut vm = VirtualMachine::new();
-    match vm.load_program("roms/ibm-logo.ch8") {
+    match vm.load_program("roms/chip8-test-rom.ch8") {
         Ok(_) => vm.run(),
         Err(e) => panic!("{:?}", e),
     }
