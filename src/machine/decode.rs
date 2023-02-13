124b2933cf4e567ab0f560b1886c2dbff052f6f5
Ian Knight <ian.knight2@nottingham.ac.uk>
2023-02-13 11:02:58 +0000

    Add GPL-3

--- src/machine/decode.rs
+++ src/machine/decode.rs
@@ -1,3 +1,16 @@
+// This file is part of rchip8.
+//
+// rchip8 is free software: you can redistribute it and/or modify it under the terms of
+// the GNU General Public License as published by the Free Software Foundation, either
+// version 3 of the License, or (at your option) any later version.
+//
+// rchip8 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with rchip8.
+// If not, see <https://www.gnu.org/licenses/>.
+
 use super::hilo::HiLo;
 use super::insts::Chip8Inst;
 use super::Chip8Machine;
